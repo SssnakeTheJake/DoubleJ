3bcd2416396d952187efda76f19b021263bc36bf
Spinark167 <jhandrews1@gmail.com>
2015-06-24 18:20:52 -0400

    Hearts
    I have added the basic heart. It moves across the screen and has a lower
    chance of spawning than the blocks do. It doesn't have a script yet as
    there is no lives GUI for it to communicate with yet.

--- Dock Blodge/Assets/Scripts/ShootBlockScript.cs
+++ Dock Blodge/Assets/Scripts/ShootBlockScript.cs
@@ -6,6 +6,7 @@ public class ShootBlockScript : MonoBehaviour {
 	public GameObject cube1;
 	public GameObject cube2;
 	public GameObject cube3;
+	public GameObject heart;
 
 	//change this in the inspector to change the initial time it takes to shoot the block
 	public float timer = 1.5f;
@@ -24,17 +25,20 @@ public class ShootBlockScript : MonoBehaviour {
 	void FixedUpdate () {
 		timer -= .02f;
 		if (timer <= 0) {
-			int random = Random.Range (1,4);
-			if (random == 1) {
+			int random = Random.Range (1,17);
+			if (random >= 1 && random < 6) {
 				Instantiate(cube1, gameObject.transform.position, gameObject.transform.rotation);
 				//GameObject cub1 = GameObject.Find("cube1");
 				//cub1.rigidbody.velocity = new Vector3(-5f, 0f, 0f);
 			};
-			if (random == 2) {
+			if (random >= 6 && random < 11) {
 				Instantiate(cube2, gameObject.transform.position, gameObject.transform.rotation);
 			};
-			if (random == 3) {
+			if (random >= 11 && random < 16) {
 				Instantiate(cube3, gameObject.transform.position, gameObject.transform.rotation);
+			};
+			if (random == 16) {
+				Instantiate (heart, gameObject.transform.position, gameObject.transform.rotation);
 			};
 
 			float random2 = Random.Range (2.25f, 3.5f);
